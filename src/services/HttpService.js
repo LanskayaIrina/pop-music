6f1a69125241d29e3919cb738ad69a2b961d9288
Irina Lanskaia <irina.lanskaia@nixsolutions.com>
2020-07-20 12:58:23 +0300

    add httpService

---
+++ src/services/HttpService.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+const baseUrl = 'http://localhost:3001/';
+
+export class HttpService {
+
+  static request = async (url, method = 'GET', body = null, options = {}) => {
+    const requestOptions = {
+      method,
+      ...options,
+      headers: {
+        'Content-type': 'application/json',
+        ...options.headers,
+      },
+    }
+
+    if (body) {
+      requestOptions.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`${baseUrl}${url}`, requestOptions);
+
+    return this.parseResponse(response);
+  }
+
+  static parseResponse = async (response) => {
+    const { ok, status, statusText } = response;
+
+    if (status === 401) {
+      return <Redirect to="/login" />;
+    }
+
+    if (ok) {
+      return response.json();
+    }
+
+    return Promise.reject(statusText);
+
+  }
+
+  static get = (url, options) => {
+
+    return this.request(url, 'GET', null, options);
+  };
+
+}
