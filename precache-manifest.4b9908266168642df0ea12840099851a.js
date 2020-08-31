2c1ed9ca64f915da6849250a871ee19d27de6ce7
Irina Lanskaia <irina.lanskaia@nixsolutions.com>
2020-08-31 11:22:15 +0300

    Updates

---
+++ precache-manifest.4b9908266168642df0ea12840099851a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c86ac8c7ee4d8e4ecd161fb8276ae8",
+    "url": "/pop-music/index.html"
+  },
+  {
+    "revision": "2291036ae4bec102a310",
+    "url": "/pop-music/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "14f6175673a1326b012d",
+    "url": "/pop-music/static/css/main.559c5948.chunk.css"
+  },
+  {
+    "revision": "2291036ae4bec102a310",
+    "url": "/pop-music/static/js/2.22e1a37d.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/pop-music/static/js/2.22e1a37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f6175673a1326b012d",
+    "url": "/pop-music/static/js/main.9b23fa24.chunk.js"
+  },
+  {
+    "revision": "b23748931ebeee7d7bb4",
+    "url": "/pop-music/static/js/runtime-main.0f3f5e97.js"
+  }
+]);
